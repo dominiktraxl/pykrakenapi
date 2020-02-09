ea6cc87430fed9cf12da4dfd1b0e80faedc6f6b7
Gonzalo Matamala <gonmator@gmail.com>
2020-02-09 11:07:05 +0100

    unit test

---
+++ test/test_pykrakenapi.py
@@ -0,0 +1,31 @@
+import unittest
+from pykrakenapi.pykrakenapi import *
+
+import krakenex as k
+import time
+
+
+class TestPykrakenapi(unittest.TestCase):
+    def test_public_callratelimiter(self):
+        raw_api = k.API()
+        pyApi = KrakenAPI(raw_api, crl_sleep=0)
+
+        # check api calls with frequency lower than 1/1s
+        time.sleep(1)
+        ldt, lut = pyApi.get_server_time()
+        for i in range(5):
+            time.sleep(1.1)
+            dt, ut = pyApi.get_server_time()
+            self.assertGreaterEqual(ut - lut, 1.0)
+            ldt = dt
+            lut = ut
+
+        # check api calls with frequency higher than 1/1s
+        for i in range(5):
+            with self.assertRaises(CallRateLimitError):
+                time.sleep(0.05)
+                dt, ut = pyApi.get_server_time()
+
+
+if __name__ == '__main__':
+    unittest.main()
