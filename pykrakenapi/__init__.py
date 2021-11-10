f9b1a031490ee0dd07e9df39674846a31fbee87a
Dominik Traxl <dominik.traxl@posteo.org>
2021-11-10 19:48:12 +0100

    bump to 0.2.2.

--- pykrakenapi/__init__.py
+++ pykrakenapi/__init__.py
@@ -72,7 +72,7 @@ from __future__ import absolute_import
 from pykrakenapi.pykrakenapi import KrakenAPI
 
 __all__ = ['KrakenAPI']
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = "Dominik Traxl <dominik.traxl@posteo.org>"
 __copyright__ = "Copyright 2017 Dominik Traxl"
 __license__ = "GNU GPL"
