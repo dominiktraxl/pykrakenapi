4ddf3ce7ed9e60a7e6838b6cdcbb6a4eaea6738a
jonnymaserati <jonnycorcutt@gmail.com>
2022-02-26 13:55:53 +0100

    Bumped the version in the __init__.py file

--- pykrakenapi/__init__.py
+++ pykrakenapi/__init__.py
@@ -72,7 +72,7 @@ from __future__ import absolute_import
 from pykrakenapi.pykrakenapi import KrakenAPI
 
 __all__ = ['KrakenAPI']
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 __author__ = "Dominik Traxl <dominik.traxl@posteo.org>"
 __copyright__ = "Copyright 2017 Dominik Traxl"
 __license__ = "GNU GPL"
